ba41cdb2bde092dd3ee83a3495d525dbab6a3e5c
Clara <liusclarabella@gmail.com>
2023-07-01 12:20:21 +0700

    -QTE scene working slider. Timer not added yet

--- MC2/Sources/App/GameViewController.swift
+++ MC2/Sources/App/GameViewController.swift
@@ -30,8 +30,8 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        presentTitleScene()
-//        presentCrashQTEScene()
-        presentMGRadioScene()
+        presentCrashQTEScene()
+//        presentMGRadioScene()
         setupGameState()
     }
     
--- MC2/Sources/Scenes/Mini Games/CrashQTEScene.swift
+++ MC2/Sources/Scenes/Mini Games/CrashQTEScene.swift
@@ -12,28 +12,49 @@ class CrashQTEScene: SKScene{
     
     var rectangle: SKSpriteNode!
     var swipeArrow: SKSpriteNode!
+    var initialArrowPosition: CGPoint = .zero
+    var isDraggingArrow = false
+
     
     override func didMove(to view: SKView) {
-        rectangle = childNode(withName: "rectangle") as? SKSpriteNode
-        swipeArrow = childNode(withName: "swipe-arrow") as? SKSpriteNode
-        
-        let swipeRight: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: Selector("swipedRight:"))
-        swipeRight.direction = .right
-        view.addGestureRecognizer(swipeRight)
+        rectangle = self.childNode(withName: "rectangle") as? SKSpriteNode
+        swipeArrow = self.childNode(withName: "swipe-arrow") as? SKSpriteNode
     }
     
-    func swipedRight(sender: UISwipeGestureRecognizer){
-        print("Object has been swiped")
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first as! UITouch
-        let location = touch.location(in: self)
-        
-        if (swipeArrow?.frame.contains(location))!{
-            print("Swipe has started")
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+              let touchLocation = touch.location(in: self)
+
+        if swipeArrow.contains(touchLocation) {
+            initialArrowPosition = swipeArrow.position
+            isDraggingArrow = true
+            print("drag arrow")
         }
     }
+   
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+              let touchLocation = touch.location(in: self)
+
+        if isDraggingArrow {
+          let minX = rectangle.frame.minX + 75
+          let maxX = rectangle.frame.maxX - 75
+          let newPosition = CGPoint(x: max(min(touchLocation.x, maxX), minX), y: swipeArrow.position.y)
+          swipeArrow.position = newPosition
+            print("swipe arrow position: \(swipeArrow.position.x)")
+        }
+    }
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isDraggingArrow = false
+        let minX = rectangle.frame.minX
+        let maxX = rectangle.frame.maxX
+        let value = (swipeArrow.position.x - minX) / (maxX - minX)
+                
+    }
+    
+
     
     
 }
